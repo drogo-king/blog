9ee9b99ff302b3f4f7c3f7020e3cefe256afc335
Wang Zicheng <bwsdmwzc1999@icloud.com>
2022-01-15 01:39:44 +0900

    nothing important

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 const util = require('util')
 const {DateTime} = require('luxon')
 const pluginRss = require("@11ty/eleventy-plugin-rss");
-const { parseHTML } = require('linkedom')
+const {parseHTML} = require('linkedom')
 
 module.exports = config => {
     config.addFilter('console', data => util.inspect(data))
     config.addFilter('readabledate', data => 
         DateTime.fromJSDate(data).toLocaleString(DateTime.DATE_FULL))
     config.addFilter('excerpt', (data, length) => {
-        const { document } = parseHTML(`<html>${data}</html>`)
+        const {document} = parseHTML(`<html>${data}</html>`)
         const text = [...document.querySelectorAll('html > p')]
             .reduce((pre, cur) => pre + cur.textContent, '')
         return text.slice(0, length) + '...'
